acd0721241a8967ea8e8f7043ff851ae75f301be
yeynii <yeynii@gmail.com>
2022-04-03 23:23:06 +0900

    amountFor 옮기기

--- chapter1/main.js
+++ chapter1/main.js
@@ -11,23 +11,13 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
   }
-}
-
-function renderPlainText(data){
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-  }
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
 
   function amountFor(aPerformance){
     let result = 0;
@@ -50,6 +40,17 @@ function renderPlainText(data){
     }
     return result;
   }
+}
+
+function renderPlainText(data){
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  return result;
 
   function volumeCreditsFor(aPerformance){
     let result = 0
@@ -78,7 +79,7 @@ function renderPlainText(data){
   function totalAmount(){
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
