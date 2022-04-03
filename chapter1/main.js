71679a0f41686823f2e2f826ec98e2d6c54b0949
yeynii <yeynii@gmail.com>
2022-04-03 23:24:48 +0900

    volumeCreditsFor 옮기기

--- chapter1/main.js
+++ chapter1/main.js
@@ -12,6 +12,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -40,6 +41,14 @@ function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance){
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" == aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data){
@@ -52,14 +61,6 @@ function renderPlainText(data){
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
-  function volumeCreditsFor(aPerformance){
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" == aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
   function usd(aNumber){
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -71,7 +72,7 @@ function renderPlainText(data){
   function totalVolumeCredits(){
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
