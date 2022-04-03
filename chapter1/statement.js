d6a09bc3b1a099c198f528c32f1a250544b548d8
yeynii <yeynii@gmail.com>
2022-04-04 00:15:24 +0900

    웹용으로 수정

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,7 +1,14 @@
-const invoices = require('./invoices.json');
-const plays = require('./plays.json');
+import createStatementData from './createStatementData.js';
+
+const { default : invoices } = await import('./invoices.json', {
+  assert :{ type: "json" }
+});
+
+const { default : plays } = await import('./plays.json', {
+  assert :{ type: "json" }
+});
+
 const invoice = invoices[0]
-const createStatementData = require('./createStatementData.js');
 
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays))
@@ -16,14 +23,33 @@ function renderPlainText(data){
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
+}
 
-  function usd(aNumber){
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber/100);
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data){
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>\n`;
+    result += `<td>적립 포인트: <em>${usd(perf.amount)}</td></tr>\n`;
   }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
+function usd(aNumber){
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber/100);
 }
 
 console.log(statement(invoice, plays));
+document.body.innerHTML = htmlStatement(invoice, plays);
