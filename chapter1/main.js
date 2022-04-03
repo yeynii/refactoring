320cfdfd731b57d7df7f48b1a4442532f54c431d
yeynii <yeynii@gmail.com>
2022-04-03 22:42:30 +0900

    함수 추출하기 변수 인라인하기

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,5 +1,6 @@
 const invoices = require('./invoices.json');
 const plays = require('./plays.json');
+const invoice = invoices[0]
 
 function amountFor(aPerformance){
   let result = 0;
@@ -43,24 +44,32 @@ function usd(aNumber){
   }).format(aNumber/100);
 }
 
+function totalVolumeCredits(){
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
+}
+
+function totalAmount(){
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += amountFor(perf);
+  }
+  return result;
+}
+
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
-
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
-  result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result
 }
 
-console.log(statement(invoices[0], plays));
+
+console.log(statement(invoice, plays));
